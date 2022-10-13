8df28a74e51ac65eddefeb15871a3e11cc370707
javiercanoes4 <javier.cano.esteban@estudiantat.upc.edu>
2022-10-13 12:17:37 +0200

    Added did resolver http api module

---
+++ didResolverApi/index.js
@@ -0,0 +1,23 @@
+import express from "express"
+import bodyParser from 'body-parser'
+
+import resolution from './resolution.js'
+
+const app = express()
+app.use(bodyParser.json())
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+)
+
+const port = 3011
+const host = "0.0.0.0"
+
+app.use("/", resolution)
+
+app.listen(port, host, () => {
+    console.log(`Example app listening at http://${host}:${port}`)
+})
+
+//module.exports = app
---
+++ didResolverApi/resolution.js
@@ -0,0 +1,16 @@
+import {getResolver} from '../didResolverModule/ereuseResolver.js'
+import  {Resolver} from 'did-resolver'
+
+import express from 'express'
+var router = express.Router();
+
+router
+
+.get('/*', async (req, res) => {
+    const eReuseResolver = getResolver("http://127.0.0.1:3010")
+    const didResolver = new Resolver(eReuseResolver)
+    const doc = await didResolver.resolve(req.url.substring(1))
+    res.send(JSON.stringify(doc, null, 2))
+})
+
+export default router;
