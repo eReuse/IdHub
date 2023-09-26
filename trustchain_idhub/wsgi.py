6e8332190bf392f9e658f604ddc5613e356a364f
Daniel Armengod <daniel@armengod.cat>
2023-09-26 09:15:28 +0200

    Started django project + first HTML render

---
+++ trustchain_idhub/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for trustchain_idhub project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trustchain_idhub.settings')
+
+application = get_wsgi_application()
