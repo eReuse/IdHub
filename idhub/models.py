29fd0aedeb0e04a6d19372e8966d71cea950d060
Daniel Armengod <daniel@armengod.cat>
2023-10-03 05:47:00 +0200

    Checkpoint

--- idhub/models.py
+++ idhub/models.py
@@ -2,9 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User as DjangoUser
 
 
-class User(DjangoUser):
+class AppUser(models.Model):
     # Ya incluye "first_name", "last_name", "email", y "date_joined" heredando de la clase User de django.
     # Falta ver que más información hay que añadir a nuestros usuarios, como los roles etc.
+    django_user = models.OneToOneField(DjangoUser, on_delete=models.CASCADE)
+
+    # Extra data, segun entidad/organizacion
     pass
 
 
@@ -14,15 +17,39 @@ class Event(models.Model):
     kind = "PLACEHOLDER"
 
 
-class DID(models.Model):
+
+
+
+
+
+
+
+
+
+
+class ExternallyStoredModel(models.Model):
+    pass
+
+    # Any models which inherit from this class are stored in wallet-kit, not in the Django ORM
+    class Meta:
+        abstract = True
+
+    @staticmethod
+    def from_json(json_serialization):
+        # Construct an instance of this class by de-serialization from data returned by wallet-kit.
+        # Must be implemented by any deriving class.
+        raise NotImplementedError()
+
+
+class DID(ExternallyStoredModel):
     did_string = models.CharField(max_length=250)
     # kind = "KEY|JWK|WEB|EBSI|CHEQD|IOTA"
 
 
-class VerifiableCredential(models.Model):
+class VerifiableCredential(ExternallyStoredModel):
     id_string = models.CharField(max_length=250)
     data = models.TextField()
     verified = models.BooleanField()
     created_on = models.DateTimeField()
-    did_issuer = models.ForeignKey(DID, on_delete=models.PROTECT)
-    did_subject = models.ForeignKey(DID, on_delete=models.PROTECT)
+    did_issuer = models.CharField(max_length=250)  # Probably not a FK but the DID directly
+    did_subject = models.CharField(max_length=250)  # Probably not a FK but the DID directly
--- trustchain_idhub/urls.py
+++ trustchain_idhub/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('django-admin/', admin.site.urls),
-    path('/', include("idhub.urls"))
+    path("accounts/login/", auth_views.LoginView.as_view()),
+    path('', include("idhub.urls"))
 ]
