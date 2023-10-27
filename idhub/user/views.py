76c7d3800266ecbda22f022f09d039ed340974f7
Cayo Puigdefabregas <cayo@puigdefabregas.eu>
2023-10-27 11:19:10 +0200

    add user dids management

--- idhub/user/views.py
+++ idhub/user/views.py
@@ -1,12 +1,15 @@
 import logging
 
 from django.utils.translation import gettext_lazy as _
-from django.views.generic.edit import UpdateView
+from django.views.generic.edit import UpdateView, CreateView, DeleteView
 from django.views.generic.base import TemplateView
+from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.contrib import messages
+from apiregiter import iota
 from idhub.user.forms import ProfileForm
 from idhub.mixins import UserView
+from idhub.models import DID
 
 
 class MyProfile(UserView):
@@ -14,7 +17,7 @@ class MyProfile(UserView):
     section = "MyProfile"
 
 
-class MyWallet(UserView, TemplateView):
+class MyWallet(UserView):
     title = _("My Wallet")
     section = "MyWallet"
 
@@ -51,25 +54,104 @@ class UserGDPRView(MyProfile, TemplateView):
     icon = 'bi bi-file-earmark-medical'
 
 
-class UserIdentitiesView(MyWallet):
-    template_name = "idhub/user/identities.html"
-    subtitle = _('Identities (DID)')
-    icon = 'bi bi-patch-check-fill'
-
-
-class UserCredentialsView(MyWallet):
+class UserCredentialsView(MyWallet, TemplateView):
     template_name = "idhub/user/credentials.html"
     subtitle = _('Credentials')
     icon = 'bi bi-patch-check-fill'
 
 
-class UserCredentialsRequiredView(MyWallet):
+class UserCredentialsRequiredView(MyWallet, TemplateView):
     template_name = "idhub/user/credentials_required.html"
     subtitle = _('Credentials required')
     icon = 'bi bi-patch-check-fill'
 
 
-class UserCredentialsPresentationView(MyWallet):
+class UserCredentialsPresentationView(MyWallet, TemplateView):
     template_name = "idhub/user/credentials_presentation.html"
     subtitle = _('Credentials Presentation')
     icon = 'bi bi-patch-check-fill'
+
+    
+class UserDidsView(MyWallet, TemplateView):
+    template_name = "idhub/user/dids.html"
+    subtitle = _('Identities (DID)')
+    icon = 'bi bi-patch-check-fill'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'dids': self.request.user.dids,
+        })
+        return context
+
+class UserDidRegisterView(MyWallet, CreateView):
+    template_name = "idhub/user/did_register.html"
+    subtitle = _('Add a new Identities (DID)')
+    icon = 'bi bi-patch-check-fill'
+    wallet = True
+    model = DID
+    fields = ('did', 'label')
+    success_url = reverse_lazy('idhub:user_dids')
+    object = None
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['initial'] = {
+            'did': iota.issue_did(),
+            'user': self.request.user
+        }
+        return kwargs
+
+    def get_form(self):
+        form = super().get_form()
+        form.fields['did'].required = False
+        form.fields['did'].disabled = True
+        return form
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        form.save()
+        messages.success(self.request, _('DID created successfully'))
+        return super().form_valid(form)
+
+
+class UserDidEditView(MyWallet, UpdateView):
+    template_name = "idhub/user/did_register.html"
+    subtitle = _('Identities (DID)')
+    icon = 'bi bi-patch-check-fill'
+    wallet = True
+    model = DID
+    fields = ('did', 'label')
+    success_url = reverse_lazy('idhub:user_dids')
+
+    def get(self, request, *args, **kwargs):
+        self.pk = kwargs['pk']
+        self.object = get_object_or_404(self.model, pk=self.pk)
+        return super().get(request, *args, **kwargs)
+
+    def get_form(self):
+        form = super().get_form()
+        form.fields['did'].required = False
+        form.fields['did'].disabled = True
+        return form
+
+    def form_valid(self, form):
+        user = form.save()
+        messages.success(self.request, _('DID updated successfully'))
+        return super().form_valid(form)
+
+
+class UserDidDeleteView(MyWallet, DeleteView):
+    subtitle = _('Identities (DID)')
+    icon = 'bi bi-patch-check-fill'
+    wallet = True
+    model = DID
+    success_url = reverse_lazy('idhub:user_dids')
+
+    def get(self, request, *args, **kwargs):
+        self.pk = kwargs['pk']
+        self.object = get_object_or_404(self.model, pk=self.pk)
+        self.object.delete()
+        messages.success(self.request, _('DID delete successfully'))
+
+        return redirect(self.success_url)
