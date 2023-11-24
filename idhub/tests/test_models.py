f25cee86c82bd9be8ee9dbd50cb7e24a97d83c93
Elijah <elijahshellsanchez@outlook.com>
2023-11-24 15:23:59 +0100

    Added tests for Admin Dashboard model, view, template, and table

---
+++ idhub/tests/test_models.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+from idhub.models import Event
+from idhub_auth.models import User
+
+
+class EventModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        user = User.objects.create(email='testuser@email.com')
+        Event.objects.create(message='Test Event', type=1, user=user)
+
+    def test_event_creation(self):
+        event = Event.objects.get(id=1)
+        self.assertEqual(event.message, 'Test Event')
+        self.assertEqual(event.get_type_name(), 'User registered')
+
+    # Add more tests for other model methods and properties
