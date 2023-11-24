ffa582f609e0ebc385a7dc16e16f8efd2bc89c07
Elijah <elijahshellsanchez@outlook.com>
2023-11-24 13:04:17 +0100

    Changed admin dashboard column names to reflect recent changes in main

--- idhub/admin/tables.py
+++ idhub/admin/tables.py
@@ -18,6 +18,10 @@ class RolesTable(tables.Table):
 
 
 class DashboardTable(tables.Table):
+    type = tables.Column(verbose_name="Event")
+    message = tables.Column(verbose_name="Description")
+    created = tables.Column(verbose_name="Date")
+
     class Meta:
         model = Event
         template_name = "idhub/custom_table.html"
