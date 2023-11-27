911ffaba99d64d3e3c31fda2d38f71560f3f8b32
Daniel Armengod <daniel@armengod.cat>
2023-11-27 07:11:39 +0100

    minor changes to verifier

--- idhub/verification_portal/models.py
+++ idhub/verification_portal/models.py
@@ -3,7 +3,8 @@ from django.db import models
 
 class VPVerifyRequest(models.Model):
     """
-    `nonce` is an opaque random string used to lookup verification requests
+    `nonce` is an opaque random string used to lookup verification requests. URL-safe.
+      Example: "UPBQ3JE2DGJYHP5CPSCRIGTHRTCYXMQPNQ"
     `expected_credentials` is a JSON list of credential types that must be present in this VP.
       Example: ["FinancialSituationCredential", "HomeConnectivitySurveyCredential"]
     `expected_contents` is a JSON object that places optional constraints on the contents of the
@@ -12,10 +13,12 @@ class VPVerifyRequest(models.Model):
     `action` is (for now) a JSON object describing the next steps to take if this verification
       is successful. For example "send mail to <destination> with <subject> and <body>"
       Example: {"action": "send_mail", "params": {"to": "orders@somconnexio.coop", "subject": "New client", "body": ...}
+    `response` is a URL that the user's wallet will redirect the user to.
     `submitted_on` is used (by a cronjob) to purge old entries that didn't complete verification
     """
     nonce = models.CharField(max_length=50)
     expected_credentials = models.CharField(max_length=255)
     expected_contents = models.TextField()
     action = models.TextField()
+    response_or_redirect = models.CharField(max_length=255)
     submitted_on = models.DateTimeField(auto_now=True)
