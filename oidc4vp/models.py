545bf76f46051271da487241f6ad97ed22050052
Cayo Puigdefabregas <cayo@usody.com>
2023-11-27 10:59:30 +0100

    merge ssi

--- oidc4vp/models.py
+++ oidc4vp/models.py
@@ -75,6 +75,12 @@ class Authorization(models.Model):
       The Verifier need to do a redirection to the user to Wallet.
       The code we use as a soft foreing key between Authorization and OAuth2VPToken.
     """
+    nonce = models.CharField(max_length=50)
+    expected_credentials = models.CharField(max_length=255)
+    expected_contents = models.TextField()
+    action = models.TextField()
+    response_or_redirect = models.CharField(max_length=255)
+
     code = models.CharField(max_length=24, default=set_code)
     created = models.DateTimeField(auto_now=True)
     presentation_definition = models.CharField(max_length=250)
@@ -142,3 +148,25 @@ class OAuth2VPToken(models.Model):
     def verifing(self):
         pass
 
+
+class VPVerifyRequest(models.Model):
+    """
+    `nonce` is an opaque random string used to lookup verification requests. URL-safe.
+      Example: "UPBQ3JE2DGJYHP5CPSCRIGTHRTCYXMQPNQ"
+    `expected_credentials` is a JSON list of credential types that must be present in this VP.
+      Example: ["FinancialSituationCredential", "HomeConnectivitySurveyCredential"]
+    `expected_contents` is a JSON object that places optional constraints on the contents of the
+      returned VP.
+      Example: [{"FinancialSituationCredential": {"financial_vulnerability_score": "7"}}]
+    `action` is (for now) a JSON object describing the next steps to take if this verification
+      is successful. For example "send mail to <destination> with <subject> and <body>"
+      Example: {"action": "send_mail", "params": {"to": "orders@somconnexio.coop", "subject": "New client", "body": ...}
+    `response` is a URL that the user's wallet will redirect the user to.
+    `submitted_on` is used (by a cronjob) to purge old entries that didn't complete verification
+    """
+    nonce = models.CharField(max_length=50)
+    expected_credentials = models.CharField(max_length=255)
+    expected_contents = models.TextField()
+    action = models.TextField()
+    response_or_redirect = models.CharField(max_length=255)
+    submitted_on = models.DateTimeField(auto_now=True)
