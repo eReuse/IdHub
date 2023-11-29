c9c507236a5da454d470aef22014b170e77716bb
Elijah <elijahshellsanchez@outlook.com>
2023-11-29 14:41:15 +0100

    Updated User management admin table

--- idhub/admin/tables.py
+++ idhub/admin/tables.py
@@ -1,14 +1,64 @@
 import django_tables2 as tables
-from django.utils.translation import gettext_lazy as _
+from django.utils.html import format_html
+
 from idhub.models import Rol, Event
 from idhub_auth.models import User
 
 
+class ButtonColumn(tables.Column):
+    attrs = {
+        "a": {
+            "type": "button",
+            "class": "text-primary",
+            "title": "'View'",
+        }
+    }
+    # django_tables will only call the render function if it doesn't find
+    # any empty values in the data, so we stop it from matching the data
+    # to any value considered empty
+    empty_values = ()
+
+    def render(self):
+        return format_html('<i class="bi bi-eye"></i>')
+
+
 class UserTable(tables.Table):
+    view_user = ButtonColumn(
+            linkify={
+                "viewname": "idhub:admin_people",
+                "args": [tables.A("pk")]
+            },
+            orderable=False,
+            )
+    membership = tables.Column(empty_values=())
+    role = tables.Column(empty_values=())
+
+    def render_membership(self, record):
+        return record.get_memberships()
+
+    def order_membership(self, queryset, is_descending):
+        # TODO: Test that this doesn't return more rows than it should
+        queryset = queryset.order_by(
+            ("-" if is_descending else "") + "memberships__type"
+        )
+
+        return (queryset, True)
+
+    def render_role(self, record):
+        return record.get_roles()
+
+    def order_role(self, queryset, is_descending):
+        queryset = queryset.order_by(
+            ("-" if is_descending else "") + "roles"
+        )
+
+        return (queryset, True)
+
     class Meta:
         model = User
         template_name = "idhub/custom_table.html"
-        fields = ("first_name", "last_name", "email", "is_active", "is_admin")
+        fields = ("last_name", "first_name", "email", "membership", "role",
+                  "view_user")
 
 
 class RolesTable(tables.Table):
