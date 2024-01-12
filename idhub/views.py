d018c46bf4b31b9f346f789c32867fb01c14825b
Cayo Puigdefabregas <cayo@usody.com>
2024-01-12 17:22:28 +0100

    fix problems with login

--- idhub/views.py
+++ idhub/views.py
@@ -13,16 +13,16 @@ class LoginView(auth_views.LoginView):
     }
 
     def get(self, request, *args, **kwargs):
-        if request.GET.get('next'):
-            self.extra_context['success_url'] = request.GET.get('next')
+        self.extra_context['success_url'] = request.GET.get(
+            'next',
+            reverse_lazy('idhub:user_dashboard')
+        )
         return super().get(request, *args, **kwargs)
 
     def form_valid(self, form):
         user = form.get_user()
         if not user.is_anonymous and user.is_admin:
-            user_dashboard = reverse_lazy('idhub:user_dashboard')
             admin_dashboard = reverse_lazy('idhub:admin_dashboard')
-            if self.extra_context['success_url'] == user_dashboard:
-                self.extra_context['success_url'] = admin_dashboard
+            self.extra_context['success_url'] = admin_dashboard
         auth_login(self.request, user)
         return HttpResponseRedirect(self.extra_context['success_url'])
