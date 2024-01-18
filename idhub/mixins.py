baa909ed94f81af654e2d2abe861afe4236ebff2
Cayo Puigdefabregas <cayo@usody.com>
2024-01-18 18:36:41 +0100

    filter request credentials if admin is not validated

--- idhub/mixins.py
+++ idhub/mixins.py
@@ -3,11 +3,20 @@ from django.contrib.auth import views as auth_views
 from django.urls import reverse_lazy, resolve
 from django.utils.translation import gettext_lazy as _
 from django.shortcuts import redirect
+from django.core.cache import cache
 
 
 class UserView(LoginRequiredMixin):
     login_url = "/login/"
     wallet = False
+
+    def get(self, request, *args, **kwargs):
+        self.admin_validated = cache.get("KEY_DIDS")
+        return super().get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        self.admin_validated = cache.get("KEY_DIDS")
+        return super().post(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -19,6 +28,7 @@ class UserView(LoginRequiredMixin):
             'path': resolve(self.request.path).url_name,
             'user': self.request.user,
             'wallet': self.wallet,
+            'admin_validated': True if self.admin_validated else False
         })
         return context
 
