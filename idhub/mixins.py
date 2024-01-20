a5d2efcd71050664a9a974a1ebf2969a86f517b7
Cayo Puigdefabregas <cayo@puigdefabregas.eu>
2024-01-20 12:26:19 +0100

    add 403 exception for bad access

--- idhub/mixins.py
+++ idhub/mixins.py
@@ -3,6 +3,21 @@ from django.contrib.auth import views as auth_views
 from django.urls import reverse_lazy, resolve
 from django.utils.translation import gettext_lazy as _
 from django.shortcuts import redirect
+from django.http import Http404
+from django.core.exceptions import PermissionDenied
+
+
+
+class Http403(PermissionDenied):
+    status_code = 403
+    default_detail = _('Permission denied. User is not authenticated')
+    default_code = 'forbidden'
+
+    def __init__(self, detail=None, code=None):
+        if detail is not None:
+            self.detail = details or self.default_details
+        if code is not None:
+            self.code = code or self.default_code
 
 
 class UserView(LoginRequiredMixin):
@@ -26,11 +41,17 @@ class UserView(LoginRequiredMixin):
 class AdminView(UserView):
 
     def get(self, request, *args, **kwargs):
-        if not request.user.is_admin:
-            url = reverse_lazy('idhub:user_dashboard')
-            return redirect(url)
+        self.check_valid_user()
+        return super().get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        self.check_valid_user()
+        return super().post(request, *args, **kwargs)
+
+    def check_valid_user(self):
+        if not self.request.user.is_admin:
+            raise Http403
 
         if self.request.session.get("2fauth"):
-            return redirect(reverse_lazy("idhub:login"))
-
-        return super().get(request, *args, **kwargs)
+            raise Http403
+        
