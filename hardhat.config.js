20b932825571beb59f72fb096a670278eedb7994
Javier Cano Esteban <javier.cano.esteban@estudiantat.upc.edu>
2024-04-10 12:15:01 +0200

    added hardhat test network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,24 @@
 require("@nomicfoundation/hardhat-ethers");
 module.exports = {
   networks:{
+    hardhat:{
+      chainId: 457,
+      gasPrice:0,
+      hardfork: "london",
+      initialBaseFeePerGas:0,
+    },
+    test: {
+      url: "http://localhost:8545",
+      accounts: {
+        mnemonic: "discover angle erosion trap barrel wage chest drop one raven tray degree",
+        path: "m/44'/60'/0'/0",
+        initialIndex: 0,
+        count: 20,
+        passphrase: "",
+      },
+      chainId: 457,
+      gasPrice: 0
+    },
     abc2_besu: {
       url: "http://45.150.187.30:8545",
       accounts: {
