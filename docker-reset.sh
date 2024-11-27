f0b4d099cc565f38e08489a902d46beb317621ea
pedro <git2021@cas.cat>
2024-11-27 01:17:44 +0100

    docker reset: point rm to proper dir

--- docker-reset.sh
+++ docker-reset.sh
@@ -21,7 +21,7 @@ main() {
 	# remove cached devices
 	rm -f observerModule/data/devices.json
         # remove old devicehub-django database
-        rm -vfr ./db/*
+        rm -vfr ./devicehub-django/db/*
 
 	# remove docker volumes (mapped filesystem mounts are persisted)
         docker compose down -v
