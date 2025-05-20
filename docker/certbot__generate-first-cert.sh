9479c1c4186ea4321f486ed12f0d626f8faf9204
pedro <git2021@cas.cat>
2025-05-21 00:44:00 +0200

    docker: read .env in certbot script

--- docker/certbot__generate-first-cert.sh
+++ docker/certbot__generate-first-cert.sh
@@ -9,6 +9,7 @@ set -u
 set -x
 
 main() {
+        . ../.env
         # inspiration: https://docs.joinpeertube.org/install/docker
         mkdir -p /opt/ereuse-docker-data/${IDHUB_DOMAIN}/certbot
         docker run -it --rm --name certbot -p 80:80 -v "$(pwd)/docker-volume/certbot/conf:/etc/letsencrypt" certbot/certbot certonly --standalone
