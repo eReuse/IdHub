ff6f220bb34d00c8ad34addd335a83b220333592
pedro <git2021@cas.cat>
2025-05-21 00:42:17 +0200

    docker: add certbot script

---
+++ docker/certbot__generate-first-cert.sh
@@ -0,0 +1,20 @@
+!/bin/sh
+
+# Copyright (c) 2025 Pedro <copyright@cas.cat>
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+set -e
+set -u
+# DEBUG
+set -x
+
+main() {
+        # inspiration: https://docs.joinpeertube.org/install/docker
+        mkdir -p /opt/ereuse-docker-data/${IDHUB_DOMAIN}/certbot
+        docker run -it --rm --name certbot -p 80:80 -v "$(pwd)/docker-volume/certbot/conf:/etc/letsencrypt" certbot/certbot certonly --standalone
+}
+
+main "${@:-}"
+
+# written in emacs
+# -*- mode: shell-script; -*-
