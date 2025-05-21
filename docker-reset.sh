5f41480ca9d54ed5c01cb28332d7822abc62b834
pedro <git2021@cas.cat>
2025-05-21 03:53:27 +0200

    docker-reset: cleanup and fix lines

--- docker-reset.sh
+++ docker-reset.sh
@@ -65,17 +65,9 @@ main() {
                 docker compose build
         fi
         docker compose up ${detach_arg:-}
-
-        # TODO docker registry
-        #project=dkr-dsg.ac.upc.edu/trustchain-oc1-orchestral
-        #idhub_image=${project}/idhub:${idhub_tag}
-        #idhub_branch=$(git -C IdHub branch --show-current)
-        # docker build -f docker/idhub.Dockerfile -t ${idhub_image} -t ${project}/idhub:${idhub_branch}__latest .
-        #docker tag hello-world:latest farga.pangea.org/pedro/test/hello-world
-        #docker push farga.pangea.org/pedro/test/hello-world:latest
 }
 
 main "${@}"
 
 # written in emacs
-# -*- mode: shell-scrip; -*-t
+# -*- mode: shell-script; -*-
