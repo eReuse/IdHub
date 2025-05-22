566d462c88631698702843aa4b2adae99ec61bcb
Cayo Puigdefabregas <cayo@puigdefabregas.eu>
2025-05-22 12:53:18 +0200

    add commands init_admin and init_org
    includes docker bindings
    
    Co-authored-by: pedro <git2021@cas.cat>

---
+++ idhub/management/commands/init_admin.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+
+class Command(BaseCommand):
+    help = "Create a admin user"
+
+    def add_arguments(self, parser):
+        parser.add_argument('email', type=str, help='email')
+        parser.add_argument('password', type=str, help='password')
+
+    def handle(self, *args, **kwargs):
+        email = kwargs['email']
+        password = kwargs['password']
+        User.objects.create_superuser(email=email, password=password)
