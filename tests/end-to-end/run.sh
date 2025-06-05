1302e6306734adc9e8ef4a5b2a56483a78abedc4
pedro <git2021@cas.cat>
2025-06-06 00:56:12 +0200

    add login-next tests (and playwright framework)

---
+++ tests/end-to-end/run.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+set -e
+set -u
+# DEBUG
+set -x
+
+main() {
+        cd "$(dirname "${0}")"
+        VIDEO="${VIDEO:-no}"
+        browser="${browser:-firefox}"
+        project="${project:-firefox}"
+        headed="${headed:---headed}"
+
+        if [ "${VIDEO}" = 'y' ]; then
+                export VIDEO
+        else
+                unset VIDEO
+        fi
+
+        if [ $# -eq 0 ]; then
+                npx playwright test --project "${project}" "${headed}"
+        else
+                #Runs playwright with specific file if provided
+                #ej. ./run "tests/lots.spec.ts"
+                for test_file in "$@"; do
+                        npx playwright test "${test_file}" --project "${project}" "${headed}"
+                done
+        fi
+}
+
+main "${@}"
+
+# written in emacs
+# -*- mode: shell-script; -*-
