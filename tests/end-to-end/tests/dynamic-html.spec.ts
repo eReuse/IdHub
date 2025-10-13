ff554113c618516e85b262d427c4530f1ba2b818
pedro <git2021@cas.cat>
2025-10-13 18:57:04 +0200

    playwright tests: put flow helpers to be reused

--- tests/end-to-end/tests/dynamic-html.spec.ts
+++ tests/end-to-end/tests/dynamic-html.spec.ts
@@ -1,50 +1,10 @@
 // TODO move this tests to different files according to the feature they cover
 
 import { test, expect } from '@playwright/test';
-
 import path from 'path';
 
-const TEST_SITE = process.env.TEST_SITE || 'http://localhost'
-
-const TEST_ADMIN_USER = process.env.TEST_ADMIN_USER || 'admin@example.org'
-const TEST_ADMIN_PASSWD = process.env.TEST_PASSWD || 'admin'
-
-const TEST_USER = process.env.TEST_USER || 'user1@example.org'
-const TEST_USER_PASSWD = process.env.TEST_PASSWD || '1234'
-
-// optional flow (only first time per idhub service start)
-async function set_org_key(page) {
-    //const encryption_key_page = page.getByText('Encryption Key')
-    const encryption_key_page_admin = await page.getByRole('heading', { name: 'Encryption Key', exact: true })
-    if (await encryption_key_page_admin.isVisible()) {
-        await page.getByPlaceholder('Key for encrypt the secrets').click();
-        await page.getByPlaceholder('Key for encrypt the secrets').fill('DEMO');
-        await page.getByRole('button', { name: 'Save' }).click();
-    }
-}
-
-async function login(page, user, password) {
-    await page.goto(`${TEST_SITE}/login`);
-    await page.getByPlaceholder('Email address').click();
-    await page.getByPlaceholder('Email address').fill(user);
-    await page.getByPlaceholder('Password').fill(password);
-    await page.getByPlaceholder('Password').press('Enter');
-}
-
-// optional page (decrypt)
-// src https://playwright.dev/docs/locators#matching-one-of-the-two-alternative-locators
-async function accept_data_protection(page) {
-        // TODO cannot be, because of this inconsistency: Data Protection (user) vs Data protection (admin)
-        //const data_protection = await page.getByRole('heading', { name: 'Data protection', exact: true })
-        const data_protection = await page.getByRole('heading', { name: 'Data protection' })
-        if (await data_protection.isVisible()) {
-                await page.locator('#id_accept_privacy').check();
-                await page.locator('#id_accept_legal').check();
-                await page.locator('#id_accept_cookies').check();
-                await page.waitForTimeout(1000);
-                await page.getByRole('link', { name: 'Confirm' }).click();
-        }
-}
+import { login, accept_data_protection, set_org_key } from './helpers/flows';
+import {TEST_SITE, TEST_ADMIN_USER, TEST_ADMIN_PASSWD, TEST_USER, TEST_USER_PASSWD} from './helpers/config'
 
 test('credential without html template', async ({ page }) => {
     test.setTimeout(0)
