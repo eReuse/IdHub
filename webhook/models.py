dee3331a443314206de940e224fc0625438d8c8b
Thomas Rusiecki <rusieckithomas@gmail.com>
2025-11-05 01:11:23 -0300

    add owner to webtoken

--- webhook/models.py
+++ webhook/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from idhub_auth.models import User
 
 # Create your models here.
 
@@ -8,3 +9,4 @@ class Token(models.Model):
     token = models.UUIDField()
     label = models.CharField(_("Label"), max_length=250, default="")
     active = models.BooleanField(_("Active"), default=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
