6b7c71f87c6a62bb295edf114a3fb33fd03e4ec9
Thomas Rusiecki <rusieckithomas@gmail.com>
2026-04-25 12:09:07 -0300

    experimental fix for pyhanko issues

--- utils/certs.py
+++ utils/certs.py
@@ -1,16 +1,16 @@
 from pyhanko.sign.signers import SimpleSigner
 from cryptography.hazmat.primitives.serialization import pkcs12
+from cryptography.hazmat.primitives import serialization
 from pyhanko_certvalidator.registry import SimpleCertificateStore
-from pyhanko.keys import _translate_pyca_cryptography_cert_to_asn1
-from pyhanko.keys import _translate_pyca_cryptography_key_to_asn1
+from asn1crypto import x509, keys
 
 
 def load_cert(pfx_bytes, passphrase):
     try:
         (
-        private_key,
-        cert,
-        other_certs_pkcs12,
+            private_key,
+            cert,
+            other_certs_pkcs12,
         ) = pkcs12.load_key_and_certificates(pfx_bytes, passphrase)
     except (IOError, ValueError, TypeError) as e:
         # logger.error(
@@ -18,18 +18,29 @@ def load_cert(pfx_bytes, passphrase):
         # )
         return None
 
-    kinfo = _translate_pyca_cryptography_key_to_asn1(private_key)
-    cert = _translate_pyca_cryptography_cert_to_asn1(cert)
-    other_certs_pkcs12 = set(
-        map(_translate_pyca_cryptography_cert_to_asn1, other_certs_pkcs12)
+    key_der = private_key.private_bytes(
+        encoding=serialization.Encoding.DER,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption()
     )
+    kinfo = keys.PrivateKeyInfo.load(key_der)
+
+    cert_der = cert.public_bytes(serialization.Encoding.DER)
+    asn1_cert = x509.Certificate.load(cert_der)
+
+    asn1_other_certs = set()
+    if other_certs_pkcs12:
+        for extra_cert in other_certs_pkcs12:
+            extra_der = extra_cert.public_bytes(serialization.Encoding.DER)
+            asn1_other_certs.add(x509.Certificate.load(extra_der))
 
     cs = SimpleCertificateStore()
-    certs_to_register = set(other_certs_pkcs12)
-    cs.register_multiple(certs_to_register)
+    if asn1_other_certs:
+        cs.register_multiple(asn1_other_certs)
+
     return SimpleSigner(
         signing_key=kinfo,
-        signing_cert=cert,
+        signing_cert=asn1_cert,
         cert_registry=cs,
         signature_mechanism=None,
         prefer_pss=False,
