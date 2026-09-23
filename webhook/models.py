f8ce3531237154ad4df91575ea334f772002d20b
Thomas Rusiecki <rusieckithomas@gmail.com>
2026-09-23 16:29:04 -0300

    make each token gave its allowed dids

--- webhook/models.py
+++ webhook/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from idhub_auth.models import User
+from idhub.models import DID
 
 # Create your models here.
 
@@ -10,3 +11,10 @@ class Token(models.Model):
     label = models.CharField(_("Label"), max_length=250, default="")
     active = models.BooleanField(_("Active"), default=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    allowed_dids = models.ManyToManyField(
+        DID,
+        blank=True,
+        related_name='tokens',
+        verbose_name=_("Allowed DIDs"),
+        help_text=_("Select the DIDs this token is permitted to use.")
+    )
